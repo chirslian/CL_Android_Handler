83020adcb69ef84d76732f23cb5baed9879d9436
chirslian <chirslian@163.com>
2015-03-21 23:49:29 +0800

    handler test

--- src/com/chirslian/cl_android_handler/MainActivity.java
+++ src/com/chirslian/cl_android_handler/MainActivity.java
@@ -1,17 +1,54 @@
 package com.chirslian.cl_android_handler;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private Button button;
+	private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        handler = new myHandler();
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//当用户点击按钮时，创建一个消息对象，并使用handler发送对象
+				Message msg = handler.obtainMessage();
+				msg.what = 23;
+				handler.sendMessage(msg);
+				//上面是将消息对象放置到消息队列当中
+				//1.Looper将会从消息队列当中取出消息对象
+				//2.Looper 将会找到与消息队列相对应的handler
+				//3.Looper将会调用handler对象的handlerMessage(Message msg)方法，用于处理消息对象
+			}
+		});
+    }
+    
+    static class myHandler extends Handler{
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			int what = msg.what;
+			System.out.println("msg.what = "+what);
+		}
+    	
     }
 
 
